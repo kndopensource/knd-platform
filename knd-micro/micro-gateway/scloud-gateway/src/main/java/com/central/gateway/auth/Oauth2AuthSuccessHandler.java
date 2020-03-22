e70d5aa95f564578bc69e5f600840fa8179f922b
w1999wtw3537@sina.com <w1999wtw3537@sina.com>
2020-03-22 15:15:48 +0800

    spring cloud gateway微服务网关

---
+++ knd-micro/micro-gateway/scloud-gateway/src/main/java/com/central/gateway/auth/Oauth2AuthSuccessHandler.java
@@ -0,0 +1,51 @@
+package com.kernel.gateway.auth;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.kernel.common.constant.SecurityConstants;
+import com.kernel.common.model.SysUser;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 认证成功处理类
+ *
+ * @author zlt
+ * @date 2019/10/7
+ * <p>
+ * Blog: https://blog.csdn.net/zlt2000
+ * Github: https://github.com/zlt2000
+ */
+public class Oauth2AuthSuccessHandler implements ServerAuthenticationSuccessHandler {
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        MultiValueMap<String, String> headerValues = new LinkedMultiValueMap(4);
+        Object principal = authentication.getPrincipal();
+        //客户端模式只返回一个clientId
+        if (principal instanceof SysUser) {
+            SysUser user = (SysUser)authentication.getPrincipal();
+            headerValues.add(SecurityConstants.USER_ID_HEADER, String.valueOf(user.getId()));
+            headerValues.add(SecurityConstants.USER_HEADER, user.getUsername());
+        }
+        OAuth2Authentication oauth2Authentication = (OAuth2Authentication)authentication;
+        String clientId = oauth2Authentication.getOAuth2Request().getClientId();
+        headerValues.add(SecurityConstants.TENANT_HEADER, clientId);
+        headerValues.add(SecurityConstants.ROLE_HEADER, CollectionUtil.join(authentication.getAuthorities(), ","));
+
+        ServerWebExchange exchange = webFilterExchange.getExchange();
+        ServerHttpRequest serverHttpRequest = exchange.getRequest().mutate()
+                .headers(h -> {
+                    h.addAll(headerValues);
+                })
+                .build();
+
+        ServerWebExchange build = exchange.mutate().request(serverHttpRequest).build();
+        return webFilterExchange.getChain().filter(build);
+    }
+}
---
+++ knd-micro/micro-gateway/scloud-gateway/src/main/java/com/central/gateway/auth/PermissionAuthManager.java
@@ -0,0 +1,46 @@
+package com.kernel.gateway.auth;
+
+import com.kernel.common.model.SysMenu;
+import com.kernel.gateway.feign.MenuService;
+import com.kernel.oauth2.common.service.impl.DefaultPermissionServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * url权限认证
+ *
+ * @author zlt
+ * @date 2019/10/6
+ * <p>
+ */
+@Slf4j
+@Component
+public class PermissionAuthManager extends DefaultPermissionServiceImpl implements ReactiveAuthorizationManager<AuthorizationContext> {
+    @Resource
+    private MenuService menuService;
+
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext authorizationContext) {
+        return authentication.map(auth -> {
+            ServerWebExchange exchange = authorizationContext.getExchange();
+            ServerHttpRequest request = exchange.getRequest();
+            boolean isPermission = super.hasPermission(auth, request.getMethodValue(), request.getURI().getPath());
+            return new AuthorizationDecision(isPermission);
+        }).defaultIfEmpty(new AuthorizationDecision(false));
+    }
+
+    @Override
+    public List<SysMenu> findMenuByRoleCodes(String roleCodes) {
+        return menuService.findByRoleCodes(roleCodes);
+    }
+}
